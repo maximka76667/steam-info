9748b1c3947a42137612c663ee2955f461e3facb
max76667 <max766667@gmail.com>
2021-08-11 13:10:41 +0600

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const [id, setId] = React.useState('');
+
+  function findUser(id: string):any {
+    return fetch('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/&steamid=76561197960434622&format=json')
+      .then((user) => {
+        console.log(user);
+        return user
+      })
+      .catch((err) => console.log(err))
+  }
+
+  function handleChange(e: any) {
+    setId(e.target.value);
+  }
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    
+    findUser(id);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="App-logo" alt="logo" />
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="text" onChange={handleChange} value={id} />
+          <button className="submit-button" type="submit">Find</button>
+        </form>
       </header>
+      <main className="content">
+
+      </main>
     </div>
   );
 }
